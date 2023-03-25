d62d6f74eee3099a81b07b69e8963a5101f27dbb
Replit user <>
2023-03-25 20:03:09 +0000

    new try

--- list.c
+++ list.c
@@ -98,7 +98,7 @@ void pushBack(List * list, void * data)
 
 void pushCurrent(List * list, void * data)
 {
-  Node *dato = createNode(data)
+  Node *dato = createNode(data);
   if (list->head == NULL && list->tail == NULL)
   {
     list->tail = dato;
